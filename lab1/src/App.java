af6ecd30cd499c60f4d299b1efd34096e7c8e338
chiieong <imlovincat@gmail.com>
2021-10-04 10:38:57 +0100

    second try

---
+++ lab1/src/App.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner (System.in);
+
+        System.out.print("Enter the length of Side A: ");
+        double sideA =  sc.nextDouble();
+        System.out.print("Enter the length of Side B: ");
+        double sideB =  sc.nextDouble();
+        System.out.print("Enter the length of Side C: ");
+        double sideC =  sc.nextDouble();
+        
+        if ((sideA + sideB) > sideC) {
+            if (sideA == sideB && sideA == sideC && sideB == sideC) {
+                System.out.print("It is a equalateral Triangle");
+            }
+            else {
+                if (sideA == sideB || sideA == sideC || sideB == sideC) {
+                    System.out.print("It is a isosceles Triangle");
+                }
+                else {
+                    System.out.print("It is a scalenes Triangle");
+                }
+            }
+        }
+        else {
+            System.out.print("It is not a Triangle ");
+        }
+    }
+}
